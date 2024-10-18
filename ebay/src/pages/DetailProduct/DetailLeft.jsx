9c7c21bb7809bc1de69d4734e35cc0c725bde6f3
Le Son <lson999920.work@gmail.com>
2024-10-19 00:51:48 +0700

    bug navigate

--- ebay/src/pages/DetailProduct/DetailLeft.jsx
+++ ebay/src/pages/DetailProduct/DetailLeft.jsx
@@ -9,24 +9,17 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 // import "../DetailProduct/style.css";
-import {
-  Navigation,
-  Pagination,
-  Mousewheel,
-  Keyboard,
-  Zoom,
-  Thumbs,
-} from "swiper/modules";
+import { Navigation, Mousewheel, Keyboard, Zoom, Thumbs } from "swiper/modules";
 import SwiperCore from "swiper";
 
 import "../DetailProduct/style.css";
 
 // Import Swiper styles
-import "swiper/css";
+// import "swiper/css";
 
 SwiperCore.use([Navigation, Thumbs]);
 const DetailLeft = () => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  // const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const mainSwiper = useRef(null);
 
   const hoverSwiper = (e) => {
@@ -34,106 +27,62 @@ const DetailLeft = () => {
   };
 
   return (
-    <div className="w-full bg-red-400 grid grid-cols-8 gap-8">
-      <div className="col-span-1 w-full">
+    <div className="w-full grid grid-cols-8 bg-red-500 gap-8">
+      {/* <div className="col-span-1 thumb-swiper">
         <Swiper
-          style={{ height: "100%", width: "100%" }}
-          onSwiper={setThumbsSwiper}
-          spaceBetween={10}
-          slidesPerView={10}
+          style={{ height: "100%", width: "100px" }}
+          // onSwiper={setThumbsSwiper}
+          spaceBetween={20}
+          // slidesPerView={5}
           autoHeight={true}
           direction="vertical"
           watchSlidesProgress={true}
         >
-          <SwiperSlide
-            data-index={0}
-            onMouseEnter={hoverSwiper}
-            style={{
-              height: "60px",
-              width: "100px",
-              backgroundColor: "black",
-            }}
-          >
+          <SwiperSlide data-index={0} onMouseEnter={hoverSwiper}>
             <img
-              style={{ height: "100%", objectFit: "contain" }}
               src="/productsPic/vivid-blurred-colorful-background.jpg"
               alt="Thumbnail 1"
             />
           </SwiperSlide>
-          <SwiperSlide
-            data-index={1}
-            onMouseEnter={hoverSwiper}
-            style={{
-              height: "60px",
-              width: "100px",
-              backgroundColor: "black",
-            }}
-          >
+          <SwiperSlide data-index={1} onMouseEnter={hoverSwiper}>
             <img
-              style={{ height: "100%", objectFit: "contain" }}
               src="/productsPic/759819a4cce0f0310e96c7c1b4167e7e.jpg"
               alt="Thumbnail 2"
             />
           </SwiperSlide>
-          <SwiperSlide
-            data-index={2}
-            onMouseEnter={hoverSwiper}
-            style={{
-              height: "60px",
-              width: "100px",
-              backgroundColor: "black",
-            }}
-          >
+          <SwiperSlide data-index={2} onMouseEnter={hoverSwiper}>
             <img
-              style={{ height: "100%", objectFit: "contain" }}
               src="/productsPic/istockphoto-1349361409-612x612.jpg"
               alt="Thumbnail 3"
             />
           </SwiperSlide>
-          <SwiperSlide
-            data-index={3}
-            onMouseEnter={hoverSwiper}
-            style={{
-              height: "60px",
-              width: "100px",
-              backgroundColor: "black",
-            }}
-          >
+          <SwiperSlide data-index={3} onMouseEnter={hoverSwiper}>
             <img
-              style={{ height: "100%", objectFit: "contain" }}
               src="/productsPic/istockphoto-1435000968-612x612.jpg"
               alt="Thumbnail 3"
             />
           </SwiperSlide>
-          <SwiperSlide
-            data-index={4}
-            onMouseEnter={hoverSwiper}
-            style={{
-              height: "60px",
-              width: "100px",
-              backgroundColor: "black",
-            }}
-          >
+          <SwiperSlide data-index={4} onMouseEnter={hoverSwiper}>
             <img
-              style={{ height: "100%", objectFit: "contain" }}
               src="/productsPic/Gradient_Background_Example.jpg"
               alt="Thumbnail 3"
             />
           </SwiperSlide>
         </Swiper>
-      </div>
-      <div className="col-span-7 w-full h-full">
-        <Swiper
+      </div> */}
+      <div className="col-span-7 h-[35rem]">
+        {/* <Swiper
           onSwiper={(swiper) => {
             mainSwiper.current = swiper;
           }}
-          thumbs={{ swiper: thumbsSwiper }}
+          // thumbs={{ swiper: thumbsSwiper }}
+          // mousewheel={true}
           cssMode={true}
           navigation={true}
-          mousewheel={true}
-          keyboard={true}
-          zoom={true}
-          modules={[Zoom, Navigation, Mousewheel, Keyboard]}
+          // keyboard={true}
+          // zoom={true}
+          // modules={[Zoom, Navigation, Mousewheel, Keyboard]}
+          modules={[Navigation]}
           className="mySwiper"
         >
           <SwiperSlide>
@@ -173,6 +122,22 @@ const DetailLeft = () => {
               <img src="/productsPic/Gradient_Background_Example.jpg" alt="" />
             </div>
           </SwiperSlide>
+        </Swiper> */}
+        <Swiper
+          navigation={true}
+          cssMode={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
         </Swiper>
       </div>
     </div>
--- ebay/src/pages/DetailProduct/DetailProduct.jsx
+++ ebay/src/pages/DetailProduct/DetailProduct.jsx
@@ -3,8 +3,8 @@ import DetailRight from "./DetailRight";
 
 const DetailProduct = () => {
   return (
-    <div className="w-full max-h-[35rem] grid grid-cols-12 gap-10">
-      <div className="w-full col-span-8">
+    <div className="w-full bg-green-500 h-[35rem] grid grid-cols-12 gap-10">
+      <div className="w-full col-span-8 h-full">
         <DetailLeft />
       </div>
       <div className="w-full col-span-4">
